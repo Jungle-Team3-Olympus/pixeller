aee738809aae17f0c6ad06de73042d68a63d238d
kssumin <201566@jnu.ac.kr>
2024-06-28 21:12:22 +0900

    feat : 초기환경 세팅

---
+++ src/test/java/com/jungle/navigation/NavigationApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jungle.navigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
