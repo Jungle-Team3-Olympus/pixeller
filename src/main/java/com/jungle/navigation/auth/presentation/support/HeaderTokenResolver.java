3eb67f7fbf764430408584ba2962538d5e1cafa9
kssumin <201566@jnu.ac.kr>
2024-07-08 16:26:06 +0900

    refactor : 토큰에서 추출해서 MemberInfo로 반환하도록 변경

--- src/main/java/com/jungle/navigation/auth/presentation/support/HeaderTokenResolver.java
+++ src/main/java/com/jungle/navigation/auth/presentation/support/HeaderTokenResolver.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class HeaderTokenResolver implements TokenResolver {
 	private static final int TOKEN_INDEX = 1;
 	private static final String MEMBER_ID = "uid";
+	private static final String MEMBER_NAME = "username";
 	private SecretKey secretKey;
 
 	public HeaderTokenResolver(@Value("${jwt.secretKey}") String secretKey) {
@@ -23,11 +24,11 @@ public class HeaderTokenResolver implements TokenResolver {
 	}
 
 	@Override
-	public Long decode(String value) {
+	public MemberInfo decode(String value) {
 		String token = parsePrefix(value);
 		Claims claims = parseClaims(token, secretKey);
 
-		return getClaimValue(claims, MEMBER_ID);
+		return getMemberInfo(claims);
 	}
 
 	private Claims parseClaims(final String token, final SecretKey secretKey) {
@@ -44,11 +45,18 @@ public class HeaderTokenResolver implements TokenResolver {
 		return Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
 	}
 
-	private Long getClaimValue(Claims claims, String claimKey) {
-		return claims.get(claimKey, Long.class);
+	private <T> T getClaimValue(Claims claims, String claimKey, Class<T> clazz) {
+		return claims.get(claimKey, clazz);
 	}
 
 	private String parsePrefix(String token) {
 		return Arrays.stream(token.split(PREFIX)).map(String::trim).toList().get(TOKEN_INDEX);
+	}
+
+	private MemberInfo getMemberInfo(Claims claims) {
+		Long memberId = getClaimValue(claims, MEMBER_ID, Long.class);
+		String memberName = getClaimValue(claims, MEMBER_NAME, String.class);
+
+		return MemberInfo.builder().memberId(memberId).memberName(memberName).build();
 	}
 }
--- src/main/java/com/jungle/navigation/auth/presentation/support/TokenResolver.java
+++ src/main/java/com/jungle/navigation/auth/presentation/support/TokenResolver.java
@@ -1,5 +1,5 @@
 package com.jungle.navigation.auth.presentation.support;
 
 public interface TokenResolver {
-	Long decode(String value);
+	MemberInfo decode(String value);
 }
