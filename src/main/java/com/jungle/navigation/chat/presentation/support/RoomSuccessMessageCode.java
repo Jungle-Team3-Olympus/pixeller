dfc3f9b97bc2b0bd67c2358da5d114508f25f2a4
kssumin <201566@jnu.ac.kr>
2024-07-08 20:47:18 +0900

    feat : room 관련 성공 코드

---
+++ src/main/java/com/jungle/navigation/chat/presentation/support/RoomSuccessMessageCode.java
@@ -0,0 +1,24 @@
+package com.jungle.navigation.chat.presentation.support;
+
+import com.jungle.navigation.common.presentation.respnose.MessageCode;
+
+public enum RoomSuccessMessageCode implements MessageCode {
+	CREATE_ROOM("채팅방 생성을 성공했습니다.", "R-001");
+	private final String message;
+	private final String code;
+
+	RoomSuccessMessageCode(String message, String code) {
+		this.message = message;
+		this.code = code;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String getCode() {
+		return code;
+	}
+}
