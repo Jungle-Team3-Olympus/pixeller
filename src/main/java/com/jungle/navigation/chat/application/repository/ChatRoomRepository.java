c0de6e4a37b4074647859e1cbaac0b7a2373e8cf
kssumin <201566@jnu.ac.kr>
2024-07-08 20:43:49 +0900

    feat : chatRoom repository 구현

---
+++ src/main/java/com/jungle/navigation/chat/application/repository/ChatRoomRepository.java
@@ -0,0 +1,13 @@
+package com.jungle.navigation.chat.application.repository;
+
+import com.jungle.navigation.chat.persistence.entity.ChatRoom;
+import com.jungle.navigation.chat.persistence.entity.RoomType;
+
+public interface ChatRoomRepository {
+
+	ChatRoom getById(Long id);
+
+	ChatRoom save(ChatRoom room);
+
+	ChatRoom findCommonChatRoom(Long senderId, Long receiverId, RoomType roomType);
+}
---
+++ src/main/java/com/jungle/navigation/chat/persistence/ChatRoomJpaRepository.java
@@ -0,0 +1,28 @@
+package com.jungle.navigation.chat.persistence;
+
+import com.jungle.navigation.chat.application.repository.ChatRoomRepository;
+import com.jungle.navigation.chat.persistence.entity.ChatRoom;
+import com.jungle.navigation.chat.persistence.entity.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ChatRoomJpaRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepository {
+
+	default ChatRoom getById(Long id) {
+		return findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 방입니다."));
+	}
+
+	@Query(
+			"SELECT DISTINCT r "
+					+ "FROM RoomMember rm "
+					+ "JOIN ChatRoom r ON rm.chatRoomId = r.id "
+					+ "WHERE r.roomType = :roomType "
+					+ "AND (rm.memberId = :senderId OR rm.memberId = :receiverId) "
+					+ "GROUP BY r.id "
+					+ "HAVING COUNT(DISTINCT rm.memberId) = 2")
+	ChatRoom findCommonChatRoom(
+			@Param("senderId") Long senderId,
+			@Param("receiverId") Long receiverId,
+			@Param("roomType") RoomType roomType);
+}
