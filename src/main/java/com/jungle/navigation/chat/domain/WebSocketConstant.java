36c73442b2b1bc06280e9a789fdbcbdcbb91e74b
kssumin <201566@jnu.ac.kr>
2024-07-08 20:27:02 +0900

    feat : websocket 상수 정의

---
+++ src/main/java/com/jungle/navigation/chat/domain/WebSocketConstant.java
@@ -0,0 +1,7 @@
+package com.jungle.navigation.chat.domain;
+
+public class WebSocketConstant {
+	public static final String SUBSCRIBE = "/sub";
+	public static final String PUBLISH = "/pub";
+	public static final String CONNECT = "/chat";
+}
