39b55aac295315e89ad09ff74bab4a1508c2a67f
kssumin <201566@jnu.ac.kr>
2024-07-12 12:30:18 +0900

    feat : 예상치 못한 exception 발생 시 slack 알람 전송하도록 구현

--- src/main/java/com/jungle/navigation/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jungle/navigation/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.jungle.navigation.common.exception;
 
+import com.jungle.navigation.common.presentation.respnose.ApiResponseBody;
+import com.jungle.navigation.common.presentation.respnose.ApiResponseBody.FailureBody;
+import com.jungle.navigation.slack.SlackAlarmGenerator;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +18,22 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+	private final SlackAlarmGenerator slackAlarmGenerator;
+
+	public GlobalExceptionHandler(SlackAlarmGenerator slackAlarmGenerator) {
+		this.slackAlarmGenerator = slackAlarmGenerator;
+	}
 
 	/** javax.validation.Valid 또는 @Validated binding error가 발생할 경우 */
 	@ExceptionHandler(BindException.class)
-	protected ResponseEntity<BindingResult> handleBindException(BindException e) {
+	public ResponseEntity<BindingResult> handleBindException(BindException e) {
 		log.warn("handleBindException", e);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getBindingResult());
 	}
 
 	/** 주로 @RequestParam enum으로 binding 못했을 경우 발생 */
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
-	protected ResponseEntity<String> handleMethodArgumentTypeMismatchException(
+	public ResponseEntity<String> handleMethodArgumentTypeMismatchException(
 			MethodArgumentTypeMismatchException e) {
 		log.warn("handleMethodArgumentTypeMismatchException", e);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
@@ -32,7 +41,7 @@ public class GlobalExceptionHandler {
 
 	/** 지원하지 않은 HTTP method 호출 할 경우 발생 */
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	protected ResponseEntity<String> handleHttpRequestMethodNotSupportedException(
+	public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(
 			HttpRequestMethodNotSupportedException e) {
 		log.warn("handleHttpRequestMethodNotSupportedException", e);
 		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(e.getMessage());
@@ -40,20 +49,26 @@ public class GlobalExceptionHandler {
 
 	/** 비즈니스 로직 실행 중 오류 발생 */
 	@ExceptionHandler(value = {BusinessException.class})
-	protected ResponseEntity<String> handleConflict(BusinessException e) {
+	public ResponseEntity<String> handleBusinessException(BusinessException e) {
 		log.warn("BusinessException", e);
-		return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 	}
 
 	/** 나머지 예외 발생 */
 	@ExceptionHandler(Exception.class)
-	protected ResponseEntity<String> handleException(Exception e) {
-		log.error("Exception", e);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+	public ResponseEntity<FailureBody> unhandledException(Exception e, HttpServletRequest request) {
+		log.error(
+				"UnhandledException: {} {} errMessage={}\n",
+				request.getMethod(),
+				request.getRequestURI(),
+				e.getMessage());
+		slackAlarmGenerator.sendAlertErrorLog(e, request);
+		return ResponseEntity.internalServerError()
+				.body(new ApiResponseBody.FailureBody(false, "server error", "서비스에 문제가 발생했습니다."));
 	}
 
 	@ExceptionHandler({MethodArgumentNotValidException.class})
-	protected ResponseEntity<String> handleMethodArgumentNotValidException(
+	public ResponseEntity<String> handleMethodArgumentNotValidException(
 			MethodArgumentNotValidException e) {
 		log.warn("MethodArgumentNotValidException", e);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
