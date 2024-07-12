1ed51e8e31de594d3ab2e6fe57a4142852a775df
kssumin <201566@jnu.ac.kr>
2024-07-12 12:29:19 +0900

    feat : exception 메시지를 받도록 변경

--- src/main/java/com/jungle/navigation/common/exception/BusinessException.java
+++ src/main/java/com/jungle/navigation/common/exception/BusinessException.java
@@ -1,13 +1,12 @@
 package com.jungle.navigation.common.exception;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public class BusinessException extends RuntimeException {
-	private final HttpStatus httpStatus;
+	private final String message;
 
-	public BusinessException(HttpStatus httpStatus) {
-		this.httpStatus = httpStatus;
+	public BusinessException(String message) {
+		this.message = message;
 	}
 }
