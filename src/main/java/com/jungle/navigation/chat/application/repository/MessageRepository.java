2b3d51ff21a3a2e3bbe2e9e946ca0486e4534662
kssumin <201566@jnu.ac.kr>
2024-07-13 21:48:47 +0900

    feat : room의 메시지를 오름차순으로 반환하는 쿼리 메서드 정의

--- src/main/java/com/jungle/navigation/chat/application/repository/MessageRepository.java
+++ src/main/java/com/jungle/navigation/chat/application/repository/MessageRepository.java
@@ -1,7 +1,13 @@
 package com.jungle.navigation.chat.application.repository;
 
 import com.jungle.navigation.chat.persistence.entity.Message;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface MessageRepository {
+	Slice<Message> findByRoomIdOrderBySendTimeAsc(Long roomId, Pageable pageable);
+
 	Message save(final Message message);
+
+	Message getById(Long id);
 }
--- src/main/java/com/jungle/navigation/chat/persistence/MessageJpaRepository.java
+++ src/main/java/com/jungle/navigation/chat/persistence/MessageJpaRepository.java
@@ -2,6 +2,16 @@ package com.jungle.navigation.chat.persistence;
 
 import com.jungle.navigation.chat.application.repository.MessageRepository;
 import com.jungle.navigation.chat.persistence.entity.Message;
+import com.jungle.navigation.common.exception.BusinessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageJpaRepository extends JpaRepository<Message, Long>, MessageRepository {}
+public interface MessageJpaRepository extends JpaRepository<Message, Long>, MessageRepository {
+
+	Slice<Message> findByRoomIdOrderBySendTimeAsc(Long roomId, Pageable pageable);
+
+	default Message getById(Long id) {
+		return findById(id).orElseThrow(() -> new BusinessException("존재하지 않는 메시지입니다."));
+	}
+}
