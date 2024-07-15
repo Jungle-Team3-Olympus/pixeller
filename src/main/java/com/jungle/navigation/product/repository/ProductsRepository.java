2423f9eb077667c96eb3594df642f83352b5de3a
Min <binine2@gmail.com>
2024-07-15 18:05:55 +0900

    fix : findAllByUseYnAndSaleYn 메서드 추가

--- src/main/java/com/jungle/navigation/product/repository/ProductsRepository.java
+++ src/main/java/com/jungle/navigation/product/repository/ProductsRepository.java
@@ -1,6 +1,10 @@
 package com.jungle.navigation.product.repository;
 
 import com.jungle.navigation.product.entity.ProductEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsRepository extends JpaRepository<ProductEntity, Integer> {}
+public interface ProductsRepository extends JpaRepository<ProductEntity, Integer> {
+
+	List<ProductEntity> findAllByUseYnAndSaleYn(char use_yn, char sale_yn);
+}
