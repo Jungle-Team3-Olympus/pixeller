02c6e025c70ac4a369f9880bbe9bae9642e72240
kssumin <201566@jnu.ac.kr>
2024-07-17 01:41:41 +0900

    feat : 특정 member의 채팅방 리스트 가져오기 api

--- src/main/java/com/jungle/navigation/chat/presentation/RoomController.java
+++ src/main/java/com/jungle/navigation/chat/presentation/RoomController.java
@@ -3,13 +3,18 @@ package com.jungle.navigation.chat.presentation;
 import com.jungle.navigation.auth.presentation.support.Member;
 import com.jungle.navigation.auth.presentation.support.MemberInfo;
 import com.jungle.navigation.chat.application.RoomService;
-import com.jungle.navigation.chat.presentation.dto.response.CreateRoomResponse;
+import com.jungle.navigation.chat.presentation.docs.RoomApi;
+import com.jungle.navigation.chat.presentation.dto.response.GetChatRoomsResponse;
+import com.jungle.navigation.chat.presentation.dto.response.RoomResponse;
 import com.jungle.navigation.chat.presentation.support.RoomSuccessMessageCode;
 import com.jungle.navigation.common.presentation.respnose.ApiResponse;
 import com.jungle.navigation.common.presentation.respnose.ApiResponseBody.SuccessBody;
 import com.jungle.navigation.common.presentation.respnose.ApiResponseGenerator;
+import com.jungle.navigation.common.presentation.respnose.SliceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,23 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class RoomController {
+public class RoomController implements RoomApi {
 
 	private final RoomService roomService;
 
-	/**
-	 * direct chat room을 가져온다
-	 *
-	 * @param memberInfo
-	 * @param oppositeId
-	 * @return
-	 */
+	@Override
 	@PostMapping("/chat-room/opposite/{oppositeId}")
-	public ApiResponse<SuccessBody<CreateRoomResponse>> joinDirectRoom(
+	public ApiResponse<SuccessBody<RoomResponse>> joinDirectRoom(
 			@Member MemberInfo memberInfo, @PathVariable("oppositeId") Long oppositeId) {
-		CreateRoomResponse response =
-				roomService.createDirectRoom(memberInfo.getMemberId(), oppositeId);
+		RoomResponse response = roomService.createDirectRoom(memberInfo.getMemberId(), oppositeId);
+
 		return ApiResponseGenerator.success(
 				response, HttpStatus.OK, RoomSuccessMessageCode.CREATE_ROOM);
+	}
+
+	@Override
+	@SubscribeMapping("/chat-room/{memberId}?page={page}&size={size}")
+	public SliceResponse<GetChatRoomsResponse> getChatRooms(
+			@DestinationVariable Long memberId,
+			@DestinationVariable int page,
+			@DestinationVariable int size) {
+		return roomService.getChatRooms(memberId, page, size);
 	}
 }
