096c5a3b6ef00533d95360e4e2bc02fddfcbfd41
kssumin <201566@jnu.ac.kr>
2024-07-18 00:35:17 +0900

    chore : alamrtype으로 클래스명 변경

--- src/main/java/com/jungle/navigation/alarm/domain/AlarmType.java
+++ src/main/java/com/jungle/navigation/alarm/domain/AlarmType.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
-public enum NotificationType {
+public enum AlarmType {
 	PURCHASE_REQUEST("purchase_request"),
 	AUCTION_START("auction_start");
 
 	private final String type;
 
-	NotificationType(String type) {
+	AlarmType(String type) {
 		this.type = type;
 	}
 
-	public static NotificationType get(String type) {
-		return Arrays.stream(NotificationType.values())
-				.filter(notificationType -> notificationType.getType().equals(type))
+	public static AlarmType get(String type) {
+		return Arrays.stream(AlarmType.values())
+				.filter(alarmType -> alarmType.getType().equals(type))
 				.findAny()
 				.orElseThrow(() -> new IllegalArgumentException("[notification type] not found"));
 	}
