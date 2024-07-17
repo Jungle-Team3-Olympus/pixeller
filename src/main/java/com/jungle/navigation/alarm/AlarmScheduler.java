2752a9b9830c42b0473b666dcc7735ce0cdf664d
kssumin <201566@jnu.ac.kr>
2024-07-17 23:28:33 +0900

    feat : 알람 시간을 계산하는 책임을 가진 클래스 생성

---
+++ src/main/java/com/jungle/navigation/alarm/AlarmScheduler.java
@@ -0,0 +1,35 @@
+package com.jungle.navigation.alarm;
+
+import com.jungle.navigation.common.helper.TimeHelper;
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Slf4j
+public class AlarmScheduler {
+	private long currentTime;
+	private long targetTime;
+
+	public AlarmScheduler(Timestamp target) {
+		this.currentTime = TimeHelper.currentTimeMillis();
+		this.targetTime = TimeHelper.toTimeMillis(target);
+
+		validateTime(currentTime, targetTime);
+	}
+
+	private void validateTime(long now, long targetTime) {
+		if (now >= targetTime) {
+			throw new IllegalArgumentException("[Alarm] Invalid time");
+		}
+	}
+
+	public String getDiff() {
+		long diffTime = targetTime - currentTime;
+		return String.valueOf(diffTime);
+	}
+}
