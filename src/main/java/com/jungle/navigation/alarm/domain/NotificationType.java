6a43c5cdda69f1f4207c777498a9c855f7f63e2b
kssumin <201566@jnu.ac.kr>
2024-07-17 23:27:24 +0900

    feat : 알람 타입 정의

--- src/main/java/com/jungle/navigation/alarm/domain/NotificationType.java
+++ src/main/java/com/jungle/navigation/alarm/domain/NotificationType.java
@@ -1,6 +1,23 @@
 package com.jungle.navigation.alarm.domain;
 
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
 public enum NotificationType {
-	PURCHASE_REQUEST,
-	AUCTION_START
+	PURCHASE_REQUEST("purchase_request"),
+	AUCTION_START("auction_start");
+
+	private final String type;
+
+	NotificationType(String type) {
+		this.type = type;
+	}
+
+	public static NotificationType get(String type) {
+		return Arrays.stream(NotificationType.values())
+				.filter(notificationType -> notificationType.getType().equals(type))
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException("[notification type] not found"));
+	}
 }
