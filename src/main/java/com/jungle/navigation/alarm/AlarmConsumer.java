d45dcf9303beaf2e6267cf50af3d51b5a44e66cd
kssumin <201566@jnu.ac.kr>
2024-07-18 01:41:35 +0900

    feat : 알람 consumer 구현

---
+++ src/main/java/com/jungle/navigation/alarm/AlarmConsumer.java
@@ -0,0 +1,33 @@
+package com.jungle.navigation.alarm;
+
+import static com.jungle.navigation.alarm.config.RMQProperties.ALARM_QUEUE_NAME;
+import static com.jungle.navigation.alarm.pub.AlarmConstant.ALARM_DESTINATION;
+
+import com.rabbitmq.client.Channel;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AlarmConsumer {
+	private final SimpMessagingTemplate messagingTemplate;
+
+	@RabbitListener(queues = ALARM_QUEUE_NAME)
+	public void listenerDelayQueue(@Payload Alarm alarm, Message message, Channel channel) {
+		log.info("[listener message] - [소비시간] - [{}] - [{}]", LocalDateTime.now(), alarm.toString());
+		try {
+			messagingTemplate.convertAndSend(ALARM_DESTINATION + "/" + alarm.getTargetId());
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+		} catch (IOException e) {
+			log.error("[listener message] :  ERROR ", e);
+		}
+	}
+}
