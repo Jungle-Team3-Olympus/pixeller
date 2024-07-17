9ebb53b8cea575f63e6828a90771c0790e98609a
kssumin <201566@jnu.ac.kr>
2024-07-17 15:22:11 +0900

    feat : 해당 room들의 멤버를 가져오는 쿼리

--- src/main/java/com/jungle/navigation/chat/application/repository/RoomMemberRepository.java
+++ src/main/java/com/jungle/navigation/chat/application/repository/RoomMemberRepository.java
@@ -1,7 +1,10 @@
 package com.jungle.navigation.chat.application.repository;
 
 import com.jungle.navigation.chat.persistence.entity.RoomMember;
+import java.util.List;
 
 public interface RoomMemberRepository {
 	RoomMember save(RoomMember roomMember);
+
+	List<RoomMember> findByChatRoomIdIn(List<Long> chatRoomIds);
 }
--- src/main/java/com/jungle/navigation/chat/persistence/RoomMemberJpaRepository.java
+++ src/main/java/com/jungle/navigation/chat/persistence/RoomMemberJpaRepository.java
@@ -2,7 +2,11 @@ package com.jungle.navigation.chat.persistence;
 
 import com.jungle.navigation.chat.application.repository.RoomMemberRepository;
 import com.jungle.navigation.chat.persistence.entity.RoomMember;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomMemberJpaRepository
-		extends JpaRepository<RoomMember, Long>, RoomMemberRepository {}
+		extends JpaRepository<RoomMember, Long>, RoomMemberRepository {
+
+	List<RoomMember> findByChatRoomIdIn(List<Long> chatRoomIds);
+}
