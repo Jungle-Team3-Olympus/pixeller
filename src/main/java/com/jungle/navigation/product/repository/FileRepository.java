29fbda81b9e099b2d631a42c93a53e8dbf6aa1da
Min <binine2@gmail.com>
2024-07-17 17:28:58 +0900

    fix : 이미지 파일 가져오기 useYn 적용

--- src/main/java/com/jungle/navigation/product/repository/FileRepository.java
+++ src/main/java/com/jungle/navigation/product/repository/FileRepository.java
@@ -5,7 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<ProductFileEntity, Integer> {
-	List<ProductFileEntity> findByProductId(int productId);
-
-	List<ProductFileEntity> findAllByProductId(int productId);
+	List<ProductFileEntity> findAllByProductIdAndUseYn(int productId, char useYn);
 }
