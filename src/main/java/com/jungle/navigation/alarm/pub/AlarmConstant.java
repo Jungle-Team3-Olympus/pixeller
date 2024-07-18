89ca1a92ce928a9479fb8b537f55f35e5553443f
kssumin <201566@jnu.ac.kr>
2024-07-18 11:38:00 +0900

    fix : 알람 수신 경로 변경

--- src/main/java/com/jungle/navigation/alarm/pub/AlarmConstant.java
+++ src/main/java/com/jungle/navigation/alarm/pub/AlarmConstant.java
@@ -1,7 +1,7 @@
 package com.jungle.navigation.alarm.pub;
 
 public class AlarmConstant {
-	private static String ALARM_DESTINATION = "sub/alarm";
+	private static String ALARM_DESTINATION = "/sub/alarm";
 
 	public static String getAlarmDestination(Long memberId) {
 		return ALARM_DESTINATION + "/" + memberId;
