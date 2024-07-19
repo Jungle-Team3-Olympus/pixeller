0689e9d17125dac104fa093e186842c498ff5a56
kssumin <201566@jnu.ac.kr>
2024-07-19 11:33:39 +0900

    feat : alarm queue에서 일정 시간동안 메시지를 처리하지 못 할 경우 deadletter queue로 전송

--- src/main/java/com/jungle/navigation/alarm/config/queue/AlarmQueueConfig.java
+++ src/main/java/com/jungle/navigation/alarm/config/queue/AlarmQueueConfig.java
@@ -3,6 +3,7 @@ package com.jungle.navigation.alarm.config.queue;
 import static com.jungle.navigation.alarm.config.RMQProperties.ALARM_EXCHANGE_NAME;
 import static com.jungle.navigation.alarm.config.RMQProperties.ALARM_QUEUE_NAME;
 import static com.jungle.navigation.alarm.config.RMQProperties.ALARM_ROUTING_KEY;
+import static com.jungle.navigation.alarm.config.RMQProperties.DEAD_LETTER_EXCHANGE_NAME;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -14,6 +15,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AlarmQueueConfig {
+	private static final String X_DEAD_LETTER_EXCHANGE_KEY = "x-dead-letter-exchange";
+	private static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+	private static final String X_MESSAGE_TTL_KEY = "x-message-ttl";
+	private static final Long X_MESSAGE_TTL = 1000 * 60 * 30L;
+
 	@Bean
 	DirectExchange alarmExchange() {
 		return new DirectExchange(ALARM_EXCHANGE_NAME);
@@ -21,7 +27,11 @@ public class AlarmQueueConfig {
 
 	@Bean
 	Queue alarmQueue() {
-		return QueueBuilder.durable(ALARM_QUEUE_NAME).build();
+		return QueueBuilder.durable(ALARM_QUEUE_NAME)
+				.withArgument(X_DEAD_LETTER_EXCHANGE_KEY, DEAD_LETTER_EXCHANGE_NAME)
+				.withArgument(X_DEAD_LETTER_ROUTING_KEY, DEAD_LETTER_EXCHANGE_NAME + ".alarm")
+				.withArgument(X_MESSAGE_TTL_KEY, X_MESSAGE_TTL)
+				.build();
 	}
 
 	@Bean
