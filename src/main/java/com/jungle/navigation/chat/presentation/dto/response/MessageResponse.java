86c468247ee918470894c0ebb4fff27996c14bc9
kssumin <201566@jnu.ac.kr>
2024-07-22 02:22:18 +0900

    fix : stomp로 response하는 것들 AbstractMessage implement 하도록 변경

--- src/main/java/com/jungle/navigation/chat/presentation/dto/response/MessageResponse.java
+++ src/main/java/com/jungle/navigation/chat/presentation/dto/response/MessageResponse.java
@@ -1,6 +1,9 @@
 package com.jungle.navigation.chat.presentation.dto.response;
 
-public record MessageResponse(boolean isBulk, Long roomId, String senderName, String message) {
+import com.jungle.navigation.chat.application.publisher.AbstractMessage;
+
+public record MessageResponse(boolean isBulk, Long roomId, String senderName, String message)
+		implements AbstractMessage {
 
 	public static MessageResponse of(Long roomId, String senderName, String message) {
 		return new MessageResponse(false, roomId, senderName, message);
--- src/main/java/com/jungle/navigation/common/presentation/respnose/SliceResponse.java
+++ src/main/java/com/jungle/navigation/common/presentation/respnose/SliceResponse.java
@@ -1,9 +1,11 @@
 package com.jungle.navigation.common.presentation.respnose;
 
+import com.jungle.navigation.chat.application.publisher.AbstractMessage;
 import java.util.List;
 
 public record SliceResponse<T>(
-		boolean isBulk, List<T> content, int currentPage, int size, boolean first, boolean last) {
+		boolean isBulk, List<T> content, int currentPage, int size, boolean first, boolean last)
+		implements AbstractMessage {
 	public static <T> SliceResponse<T> of(
 			List<T> content, int page, int size, boolean first, boolean last) {
 		return new SliceResponse<>(true, content, page, size, first, last);
