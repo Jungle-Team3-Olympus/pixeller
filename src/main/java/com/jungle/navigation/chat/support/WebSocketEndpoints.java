182c58c200e7635c82f643198b61abd1091271aa
kssumin <201566@jnu.ac.kr>
2024-07-21 23:21:48 +0900

    feat : websocketEndpoint 관련 함수 한 클래스로 모아둠

--- src/main/java/com/jungle/navigation/chat/support/WebSocketEndpoints.java
+++ src/main/java/com/jungle/navigation/chat/support/WebSocketEndpoints.java
@@ -5,11 +5,18 @@ public class WebSocketEndpoints {
 	public static final String PUBLISH = "/pub";
 	public static final String CONNECT = "/chat";
 
+	public static final String PUBLIC_ROOM_UUID = "1";
+	public static final String SUB_PUBLIC_ROOM = SUBSCRIBE + "/message/" + PUBLIC_ROOM_UUID;
+
 	public static String getDirectMessageDestination(Long roomId) {
 		return SUBSCRIBE + "/message/direct/" + roomId;
 	}
 
 	public static String getChatRoomsDestination(Long memberId) {
 		return SUBSCRIBE + "/chat-room/" + memberId;
+	}
+
+	public static String getSubInfoDestination(Long memberId) {
+		return SUBSCRIBE + "/chat-room/info/" + memberId;
 	}
 }
