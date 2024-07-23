74a65108209e9c33a1ebffc9150ed73a0f39f553
kssumin <201566@jnu.ac.kr>
2024-07-24 04:21:00 +0900

    fix : 멤버 관련 필요 정보만 넘기도록 변경

--- src/main/java/com/jungle/navigation/product/dto/RequestEditDto.java
+++ src/main/java/com/jungle/navigation/product/dto/RequestEditDto.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public record RequestEditDto(
 		@NotBlank(message = "상품이름을 기입해주세요.") String name,
-		@NotBlank(message = "카테고리를 기입해주세요.") String category,
 		@Min(value = 1000, message = "상품의 최소 가격은 1000원 입니다.") @NotNull(message = "원하시는 최소 가격을 책정해주세요.")
 				int price,
 		@Size(min = 10, max = 50, message = "최소 10자 이상 최대 50자 이하입니다.")
--- src/main/java/com/jungle/navigation/product/dto/ResponseProductWithImageUrlDto.java
+++ src/main/java/com/jungle/navigation/product/dto/ResponseProductWithImageUrlDto.java
@@ -1,6 +1,5 @@
 package com.jungle.navigation.product.dto;
 
-import java.sql.Timestamp;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,25 +10,12 @@ public class ResponseProductWithImageUrlDto {
 
 	private int productId;
 	private String name;
-	private String category;
 	private double price;
 	private String description;
 	private char saleYn;
 	private char useYn;
 	private List<String> imageFileUrls;
 	private MemberDto memberDto;
-	private Timestamp auctionStartTime;
 
-	public static record MemberDto(
-			int memberId,
-			String id,
-			char userType,
-			float x,
-			float y,
-			Timestamp lastLogin,
-			Timestamp joinedAt,
-			String username,
-			String email,
-			String direction,
-			String googleIdentity) {}
+	public record MemberDto(int memberId, String id) {}
 }
