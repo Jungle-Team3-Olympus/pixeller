0e1fb4a263d1ad5dd8063bcabece2b10ff01a2fe
kssumin <201566@jnu.ac.kr>
2024-07-24 04:24:19 +0900

    chore : s3위치 변경

--- src/main/java/com/jungle/navigation/external/s3/S3Config.java
+++ src/main/java/com/jungle/navigation/external/s3/S3Config.java
@@ -1,4 +1,4 @@
-package com.jungle.navigation.product.config;
+package com.jungle.navigation.external.s3;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
